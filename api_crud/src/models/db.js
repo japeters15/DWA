db4d0be8b69a22c9a0843be4e774197e4c32799f
Janay Peters <japeters15@gmail.com>
2017-03-11 17:43:00 -0500

    style_guide
    had issues saving my style_guide branch files

--- api_crud/src/models/db.js
+++ api_crud/src/models/db.js
@@ -1,12 +1,12 @@
-var Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 if (!process.env.NODE_ENV) require('dotenv').config();
 
-var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_SCHEMA,
   port: process.env.DB_PORT,
 
-  //connections happening to the system
+  // connections happening to the system
   pool: {
     max: 5,
     min: 0,
@@ -16,7 +16,7 @@ var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.
 });
 
 // Sequelize will create the table
-var url = sequelize.define('url', {
+const url = sequelize.define('url', {
   name: {
     type: Sequelize.STRING,
   },
--- api_crud/src/routes/api/url.js
+++ api_crud/src/routes/api/url.js
@@ -1,51 +1,51 @@
-var url = require('../../models/url.js')
-var util = require('../../../lib/util.js')
+const url = require('../../models/url.js');
+const util = require('../../../lib/util.js');
 
 module.exports = (express) => {
-  var router = express.Router();
+  const router = express.Router();
 
-//create
-router.get('/urls', (req, res) => {
-  url.findAll( ((err) => {
-      util.debug("Error: Someone tried accessing all urls", err, 'error');
-    res.status(500).json(err);
-  }), (data) => {
-    util.debug("Someone accessed all urls", data, 'success');
-    res.status(200).json(data);
-  })
-});
+// create
+  router.get('/urls', (req, res) => {
+    url.findAll(((err) => {
+      util.debug('Error: Someone tried accessing all urls', err, 'error');
+      res.status(500).json(err);
+    }), (data) => {
+      util.debug('Someone accessed all urls', data, 'success');
+      res.status(200).json(data);
+    });
+  });
 
 // Read One
   router.get('/urls/:id', (req, res) => {
     req.body.id = req.params.id;
-    user.find(req.body, (err) => {
+    url.find(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
-      util.debug("Someone accessed one url", data);
+      util.debug('Someone accessed one url', data);
       res.status(200).json(data);
-    })
+    });
   });
 
 
   // Delete
   router.delete('/urls/:id', (req, res) => {
     req.body.id = req.params.id;
-    user.destroy(req.body, (err) => {
+    url.destroy(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
   // Update
   router.post('/urls/:id', (req, res) => {
     req.body.id = req.params.id;
-    user.update(req.body, (err) => {
+    url.update(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
   return router;
-}
+};
